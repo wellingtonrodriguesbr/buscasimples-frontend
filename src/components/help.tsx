6c8f32e56fd819c82b656e9032fea80216a40c47
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-07-30 11:30:45 -0300

    update

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -1,14 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import { ArrowLeft, Copy } from "lucide-react";
 import { Button } from "./ui/button";
-import Link from "next/link";
 
 export function Help() {
+  const router = useRouter();
+
   return (
     <section className="w-full mt-8 max-w-screen-md px-4 mx-auto">
-      <Link href="/minha-conta" className="flex items-center gap-2 w-fit mb-8">
+      <Button variant="link" onClick={() => router.back()} className="mb-8">
         <ArrowLeft className="size-4" />
         Voltar
-      </Link>
+      </Button>
       <header className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-14">
         <h1 className="font-semibold text-2xl md:text-3xl">
           Precisamos da sua ajuda para manter este projeto no ar
